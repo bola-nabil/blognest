7195544a4cb75449ef0e26dd5f0d38139966aba3
Bola Nabil <bolanabil404@gmail.com>
2026-02-22 12:16:10 +0200

    make icons file to include all icons in project

---
+++ src/utils/icons.js
@@ -0,0 +1,51 @@
+import 
+{ 
+    faBookmark,
+    faHeart,
+    faComment,
+    faTrash,
+    faEdit,
+    faSave,
+    faTimes,
+    faMagnifyingGlass,
+    faBars,
+    faXmark,
+    faHouse,
+    faFire,
+    faTags,
+    faNewspaper,
+    faRightToBracket,
+    faGear,
+    faPen,
+    faUser,
+    faLock,
+    faPalette
+}
+from "@fortawesome/free-solid-svg-icons";
+
+import {faBookmark as regularBookmark } from "@fortawesome/free-regular-svg-icons"
+
+
+export {
+    faBookmark,
+    faHeart,
+    faComment,
+    faTrash,
+    faEdit,
+    faSave,
+    faTimes,
+    regularBookmark,
+    faMagnifyingGlass,
+    faBars,
+    faXmark,
+    faHouse,
+    faFire,
+    faTags,
+    faNewspaper,
+    faRightToBracket,
+    faGear,
+    faPen,
+    faUser,
+    faLock,
+    faPalette
+};
