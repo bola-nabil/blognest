ff534e882cbcfb1eff831df94fe1d55d6c524b77
Bola Nabil <bolanabil404@gmail.com>
2026-02-25 17:32:17 +0200

    make component for loading

--- src/components/notifications/NotificationsContent.jsx
+++ src/components/notifications/NotificationsContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNotifications } from '@/hooks/useNotifications';
 import { api } from '@/api';
+import NotificationsLoading from '../Loading/NotificationsLoading';
 
 const NotificationsContent = () => {
   const { notifications, setNotifications, loading } = useNotifications();
@@ -30,25 +31,7 @@ const NotificationsContent = () => {
   };
 
   if (loading) {
-    return (
-      <div className="animate-pulse space-y-6">
-        <div className="flex items-center justify-between">
-          <div className="h-8 w-40 bg-gray-300 dark:bg-gray-700 rounded"></div>
-          <div className="h-8 w-32 bg-gray-300 dark:bg-gray-700 rounded"></div>
-        </div>
-
-        {[...Array(4)].map((_, i) => (
-          <div
-            key={i}
-            className="p-4 border rounded-xl shadow-sm bg-gray-100 dark:bg-gray-800"
-          >
-            <div className="h-5 w-32 bg-gray-300 dark:bg-gray-700 rounded mb-2"></div>
-            <div className="h-4 w-60 bg-gray-200 dark:bg-gray-700 rounded mb-2"></div>
-            <div className="h-3 w-24 bg-gray-200 dark:bg-gray-700 rounded"></div>
-          </div>
-        ))}
-      </div>
-    );
+    return <NotificationsLoading />
   }
 
   return (
