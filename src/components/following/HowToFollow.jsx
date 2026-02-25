9b00b47e9c03029f6af43b0d6afdc7f4ac75d020
Bola Nabil <bolanabil404@gmail.com>
2026-02-25 17:39:03 +0200

    add Loading component

--- src/components/following/HowToFollow.jsx
+++ src/components/following/HowToFollow.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import { useUsers } from '@/hooks/useUsers';
 import { useFollow } from '@/hooks/useFollow';
+import FollowLoading from '../Loading/FollowLoading';
 
 const HowToFollow = () => {
   const { users, loading } = useUsers();
 
   if (loading) {
-    return (
-      <div className="space-y-4">
-        {[...Array(3)].map((_, i) => (
-          <div
-            key={i}
-            className="flex items-center justify-between p-4 border rounded-xl animate-pulse"
-          >
-            <div className="flex items-center gap-3">
-              <div className="w-10 h-10 bg-gray-200 rounded-full" />
-              <div className="space-y-2">
-                <div className="w-24 h-3 bg-gray-200 rounded" />
-                <div className="w-16 h-3 bg-gray-100 rounded" />
-              </div>
-            </div>
-            <div className="w-20 h-7 bg-gray-200 rounded-full" />
-          </div>
-        ))}
-      </div>
-    );
+    return <FollowLoading />
   }
 
   if (users.length === 0) {
