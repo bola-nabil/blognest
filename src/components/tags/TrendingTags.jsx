bf2cf8f2febf0bb854a731794591ab548fdee212
Bola Nabil <bolanabil404@gmail.com>
2026-02-26 11:45:05 +0200

    loading of treending tags

--- src/components/tags/TrendingTags.jsx
+++ src/components/tags/TrendingTags.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { useTrendingTags } from '@/hooks/useTrendingTags';
 import { Link } from 'react-router-dom';
+import { TrendingTagsLoading } from '@/components/Loading';
 
 const TrendingTags = () => {
   const { tags, loading } = useTrendingTags();
 
   if (loading) {
-    return (
-      <div className="flex flex-wrap gap-2">
-        {[...Array(6)].map((_, i) => (
-          <div
-            key={i}
-            className="h-8 w-20 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 animate-pulse rounded-full"
-          ></div>
-        ))}
-      </div>
-    );
+    return <TrendingTagsLoading />;
   }
 
   if (!tags.length) {
